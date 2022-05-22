88352a0e2448fba9163de483e3b2948bc7ebc3c1
Yusuf Aydoğan <79870923+MrCodeEngineer@users.noreply.github.com>
2022-05-22 19:42:57 +0300

    Create gelişmiş qrcode oluşturucu.py

---
+++ gelişmiş qrcode oluşturucu.py
@@ -0,0 +1,19 @@
+#öncelikle terminale gelip kütüphaneyi kurun: "pip install qrcode"
+
+import qrcode
+
+qr = qrcode.QRCode(
+    version=1, #1'den 40'a kadar versiyon kullanabilirsiniz.
+    error_correction = qrcode.constants.ERROR_CORRECT_L, 
+    box_size=10, #kutucuk boyutu
+    border=2 #kenar boşluğu
+    )
+
+gidenDeger = "İstediğiniz metin veya linki girin."
+
+qr.add_data(gidenDeger)
+qr.make(fit=True)
+
+resim = qr.make_image(fill_color="red", back_color="blue") 
+#Yukarıdaki satırda olduğu gibi istediğiniz ana ve arkaplan rengini kullanabilirsiniz.
+resim.save("advenceQR.png") #Ve son olarak istediğiniz isimde kaydedebilirsiniz.
